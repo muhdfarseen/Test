dff0911728dd23862b5e7a1a31c164f04a723af6
Farseen <muhdfarseen@gmail.com>
2024-06-30 20:21:51 +0530

    my swantham templete

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import { Flex } from "@mantine/core";
+
+function App() {
+  return (
+    <>
+      <Flex align={"center"} justify={"center"} h={"100dvh"}>
+        My Swantham Templete
+      </Flex>
+    </>
+  );
+}
+
+export default App;
