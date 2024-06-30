67e7ebc0395ff1312c10bed4963b4e231b63c92f
Farseen <muhdfarseen@gmail.com>
2024-06-30 20:37:35 +0530

    folder alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/',
+    preview: {
+        port: 3000,
+        strictPort: true,
+    },
+    server: {
+        port: 3000,
+        strictPort: true,
+        host: true,
+        origin: 'http://0.0.0.0:3000',
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@app': path.resolve(__dirname, './src/app'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@config': path.resolve(__dirname, './src/config'),
+            '@providers': path.resolve(__dirname, './src/providers'),
+            '@routes': path.resolve(__dirname, './src/routes'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+        },
+    },
+    plugins: [react()],
 });
